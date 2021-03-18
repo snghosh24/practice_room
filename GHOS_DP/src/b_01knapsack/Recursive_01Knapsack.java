d913161c4f3ff3cc7db33b4f1f4b67affa2a147f
snghosh24 <snehashis.co.in@gmail.com>
2021-03-18 13:49:05 +0530

    recursive way

---
+++ GHOS_DP/src/b_01knapsack/Recursive_01Knapsack.java
@@ -0,0 +1,32 @@
+package b_01knapsack;
+
+public class Recursive_01Knapsack {
+
+	static int max(int a, int b) {
+		return (a > b) ? a : b;
+	}
+
+	public static int knapsack(int w[], int val[], int W, int n) {
+
+		if (n == 0 || W == 0) {
+			return 0;
+		}
+
+		if (w[n - 1] <= W) {
+			return max(val[n - 1] + knapsack(w, val, W - w[n - 1], n - 1), knapsack(w, val, W, n - 1));
+		} else {
+			return knapsack(w, val, W, n - 1);
+		}
+	}
+
+	public static void main(String[] args) {
+
+		int w[] = { 2, 4, 2, 5, 1 };
+		int val[] = { 5, 3, 4, 2, 7 };
+		int W = 7;
+		int n = val.length;
+		
+		System.out.println("Max profit="+knapsack(w, val, W, n));
+	}
+
+}
